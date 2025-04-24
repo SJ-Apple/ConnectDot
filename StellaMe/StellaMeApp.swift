67ab6b20a19bd832b47ed8c47b2e4381d66dedaa
Hazel <hazel@10-141-51-110.0200.pos.kr.iosda.org>
2025-04-24 15:41:52 +0900

    Initial Xcode project

---
+++ StellaMe/StellaMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StellaMeApp.swift
+//  StellaMe
+//
+//  Created by Hazel on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct StellaMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
