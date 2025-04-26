92c33452e95d2306c339f52c5c3464f3e1fa220f
Hwi <cksgnl0523@gmail.com>
2025-04-26 09:35:38 +0900

    feat: add StarView to enable tapping stars for navigation

--- StellaMe/Star/StarView.swift
+++ StellaMe/Star/StarView.swift
@@ -4,15 +4,36 @@
 //
 //  Created by Hwi on 4/26/25.
 //
-
+// 클릭할 수 있는 객체로의 Star
 import SwiftUI
+import SwiftData
 
 struct StarView: View {
+    let star: StarModel
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationLink(destination: StarDetailView(star: star)) {
+            VStack {
+                // TODO: 이미지 설정, 디자인 바꾸기
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(.yellow)
+                    .padding(10)
+                    .background(Color.gray.opacity(0.2))
+                    .clipShape(Circle())
+
+                Text(star.starText)
+                    .font(.caption)
+                    .foregroundColor(.primary)
+            }
+            .padding()
+        }
+        .buttonStyle(.plain) // 기본 스타일 제거
     }
 }
 
+// 프리뷰
 #Preview {
-    StarView()
+    StarView(star: StarModel(starID: UUID(), starText: "", date: Date()))
 }
